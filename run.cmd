3ce9d5f8b473aa26a09c5848db84fb23f7ea53c4
Faisal Fida <FA20-BCS-004@cuiwah.edu.pk>
2023-11-16 21:16:01 +0500

    run

--- run.cmd
+++ run.cmd
@@ -4,5 +4,7 @@ python -m venv venv
 venv\Scripts\activate
 pip install -r requirements.txt
 python manage.py runserver
+python manage.py makemigrations
+python manage.py migrate
 
 start cmd /k "cd frontend && ng serve"
