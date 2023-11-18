8097e8a3b487d1819ac0914508f948f126480277
Faisal Fida <FA20-BCS-004@cuiwah.edu.pk>
2023-11-18 22:40:39 +0500

    auth

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from api import urls
 from django.conf.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("dj_rest_auth.urls")),
-    path("api/registration/", include("dj_rest_auth.registration.urls")),
+    re_path("^auth/", include("djoser.urls")),
+    re_path("^auth/", include("djoser.urls.jwt")),
 ]
