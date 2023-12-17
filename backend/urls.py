6e44729572c1545efb8d123c3280c064587a7ddc
Faisal Fida <FA20-BCS-004@cuiwah.edu.pk>
2023-12-18 01:56:57 +0500

    up

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", include("api.urls")),
+    path("api/", include("api.urls")),
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
     path("auth/", include("users.urls")),
